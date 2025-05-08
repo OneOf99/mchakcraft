cd4d2c9dddd80fcee9abcd8f3e054b6775a66487
OneOf99 <minimoldorm@gmail.com>
2025-05-07 21:55:26 -0400

    First real addition, item from tutorial

--- src/main/java/com/mchak/mchakcraft/Mchakcraft.java
+++ src/main/java/com/mchak/mchakcraft/Mchakcraft.java
@@ -3,9 +3,10 @@ package com.mchak.mchakcraft;
 import net.fabricmc.api.ModInitializer;
 
 public class Mchakcraft implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("FEGLI");
+        ModItems.initialize();
 
-    @Override
-    public void onInitialize() {
-        System.out.println("FEGLI");
-    }
+	}
 }
--- src/main/java/com/mchak/mchakcraft/ModItems.java
+++ src/main/java/com/mchak/mchakcraft/ModItems.java
@@ -1,4 +1,35 @@
 package com.mchak.mchakcraft;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
 public class ModItems {
+
+	public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
+
+	public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+		// Create the item key
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("mchakcraft", name));
+
+		// Create the item instance
+		Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+		// Register the item
+		Registry.register(Registries.ITEM, itemKey, item);
+
+		return item;
+	}
+
+	public static void initialize() {
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+				.register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+	}
 }
